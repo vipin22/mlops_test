72e8523c64bd97e66f3a0205f1758fa8a2322e04
Vipin Kumar Sharma <vsharma@radisys.com>
2024-06-22 08:26:59 +0530

    Adding cookiecutter template

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='We want to test mlops end to end pipeline',
+    author='Vipin',
+    license='MIT',
+)
